18197a73ee855478771b4e23811c5edd3aa8c569
Andrew Hsieh <andrewhsieh@google.com>
2013-06-11 19:14:33 +0800

    [ndk][x86] Fix clang -target *-linux-android long double to 64-bit
    To be consistent with Android and its GCC counterpart
    i686-linux-android-gcc
    
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -3653,10 +3653,16 @@ class X86_32TargetInfo : public X86TargetInfo {
 public:
   X86_32TargetInfo(const llvm::Triple &Triple) : X86TargetInfo(Triple) {
     DoubleAlign = LongLongAlign = 32;
-    LongDoubleWidth = 96;
     LongDoubleAlign = 32;
     SuitableAlign = 128;
-    DataLayoutString = "e-m:e-p:32:32-f64:32:64-f80:32-n8:16:32-S128";
+    if (Triple.getEnvironment() == llvm::Triple::Android) {
+      LongDoubleWidth = 64;
+      LongDoubleFormat = &llvm::APFloat::IEEEdouble;
+      DataLayoutString = "e-m:e-p:32:32-f64:32:64-n8:16:32-S128";
+    } else {
+      LongDoubleWidth = 96;
+      DataLayoutString = "e-m:e-p:32:32-f64:32:64-f80:32-n8:16:32-S128";
+    }
     SizeType = UnsignedInt;
     PtrDiffType = SignedInt;
     IntPtrType = SignedInt;
