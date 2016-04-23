hsz0707/clang

[ndk] Add a flag to disable C++ missing return semantics.
Conflicts:

	include/clang/Driver/Options.td

Change-Id: Id8125986a3f6d1326f9c2fb3ce19ef886104cca8

lib/CodeGen/CodeGenFunction.cpp
@@ -753,7 +753,8 @@ void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
   //   If the '}' that terminates a function is reached, and the value of the
   //   function call is used by the caller, the behavior is undefined.
   if (getLangOpts().CPlusPlus && !FD->hasImplicitReturnZero() &&
-      !FD->getResultType()->isVoidType() && Builder.GetInsertBlock()) {
+      !FD->getResultType()->isVoidType() && Builder.GetInsertBlock() &&
+      getLangOpts().CXXMissingReturnSemantics) {
     if (SanOpts->Return)
       EmitCheck(Builder.getFalse(), "missing_return",
                 EmitCheckSourceLocation(FD->getLocation()),

crystax/android-toolchain-clang-3-8

Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

lib/CodeGen/CodeGenFunction.cpp
@@ -1009,7 +1009,8 @@ void CodeGenFunction::GenerateCode(GlobalDecl GD, llvm::Function *Fn,
   //   If the '}' that terminates a function is reached, and the value of the
   //   function call is used by the caller, the behavior is undefined.
   if (getLangOpts().CPlusPlus && !FD->hasImplicitReturnZero() && !SawAsmBlock &&
-      !FD->getReturnType()->isVoidType() && Builder.GetInsertBlock()) {
+      !FD->getReturnType()->isVoidType() && Builder.GetInsertBlock() &&
+      getLangOpts().CXXMissingReturnSemantics) {
     if (SanOpts.has(SanitizerKind::Return)) {
       SanitizerScope SanScope(this);
       llvm::Value *IsFalse = Builder.getFalse();